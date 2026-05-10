d51f784f8e0acb2866745f5ffc5b08ab14581261
codebreaker7897 <codebreaker7897@gmail.com>
2026-05-11 00:53:09 +0600

    Create responsive banner component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './assets/components/Navbar'
+import Banner from './assets/components/Banner'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <Navbar></Navbar>
+    <Banner></Banner>
     
 <h1 className='text-red-600 text-bold text-4xl text-center '>Hello word</h1>
     </>
