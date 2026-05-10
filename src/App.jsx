6e682b97832840c49aca521d594179127418d7f3
codebreaker7897 <codebreaker7897@gmail.com>
2026-05-11 00:34:01 +0600

    Added by NavBar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import Navbar from './assets/components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar></Navbar>
+    
+<h1 className='text-red-600 text-bold text-4xl text-center '>Hello word</h1>
+    </>
+  )
+}
+
+export default App
